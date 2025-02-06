50d2160a1422cbc6799009749524450829d5ca69
Abishekdb <dbtelegrambots@gmail.com>
2025-02-06 21:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "8ded77e28e1eb760ca30704bc4a6eeaf")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "testingforward12345bot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dbtelegrambots4:<db_password>@cluster0.frwrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dbtelegrambots5:0N5nCg9cnhMnCRMw@cluster0.su3u7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7562335183"))
 
