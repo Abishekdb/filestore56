ae7a6ca94ff4da4c63e84ac5a51a5b0281bfccea
Abishekdb <dbtelegrambots@gmail.com>
2025-02-06 19:38:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
     API_ID = int(environ.get("API_ID", "20842824"))
     API_HASH = environ.get("API_HASH", "8ded77e28e1eb760ca30704bc4a6eeaf")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_SESSION = environ.get("BOT_SESSION", "testingforward12345bot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dbtelegrambots4:<db_password>@cluster0.frwrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7562335183"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
