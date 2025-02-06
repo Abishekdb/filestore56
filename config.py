b0c2149883260e968bec91e116ae4b4869044ab1
Abishekdb <dbtelegrambots@gmail.com>
2025-02-06 19:34:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "20842824"))
+    API_HASH = environ.get("API_HASH", "8ded77e28e1eb760ca30704bc4a6eeaf")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
